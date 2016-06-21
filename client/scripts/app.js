4d1e38662b7b13f75cf7bdbe71586d3925a40542
rhanzawa <rhanzawa@gmail.com>
2016-06-21 01:59:27 -0700

    Refactors app.js to fit testing interface

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,78 +1,79 @@
-(function() {
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   const REFRESH_INTERVAL = 10000;
-  const SERVER_URL = 'https://api.parse.com/1/classes/messages';
 
   let messageList;
   let latestMessageCreatedAt = '2000-01-11T00:00:01.000Z';
 
-  const init = function() {
-    getMessagesFromServer();
-    setInterval(getMessagesFromServer, REFRESH_INTERVAL);
+  window.app = {
+    server: 'https://api.parse.com/1/classes/messages',
 
-    $('#submit-message').on('click', handleMessageSubmit);
-  };
+    init: function() {
+      app.fetch();
+      setInterval(app.fetch, REFRESH_INTERVAL);
 
-  const handleMessageSubmit = function() {
-    const params = window.location.search;
-    const username = params.slice(params.indexOf('username=') + 9);
-    const text = $('#message-input').val();
-    postMessageToServer(username, text, 'HR');
-  };
+      $('#submit-message').on('click', app.handleMessageSubmit);
+    },
 
-  const getMessagesFromServer = function() {
-    $.ajax({
-      url: SERVER_URL,
-      type: 'GET',
-      data: {where: {createdAt: {'$gt': latestMessageCreatedAt}}},
-      dataType: 'json',
-      success: function(data) {
-        if (data.results.length > 0) {
-          messageList = escapeArrayOfObjects(data.results);
-          renderMessageList();
-          console.log('chatterbox: message list retrieved.');
+    handleMessageSubmit: function() {
+      const params = window.location.search;
+      const username = params.slice(params.indexOf('username=') + 9);
+      const text = $('#message-input').val();
+      app.send({username, text, roomname: 'HR'});
+    },
+
+    fetch: function() {
+      $.ajax({
+        url: app.server,
+        type: 'GET',
+        data: {where: {createdAt: {'$gt': latestMessageCreatedAt}}},
+        dataType: 'json',
+        success: function(data) {
+          if (data.results.length > 0) {
+            messageList = app.escapeArrayOfObjects(data.results);
+            app.renderMessageList();
+            console.log('chatterbox: message list retrieved.');
+          }
+        },
+        error: function(data) {
+          console.error('chatterbox: Failed to retrieve message list', data);
         }
-      },
-      error: function(data) {
-        console.error('chatterbox: Failed to retrieve message list', data);
+      });
+    },
+
+    send: function(messageObj) {
+      $.ajax({
+        url: app.server,
+        type: 'POST',
+        data: JSON.stringify(messageObj),
+        contentType: 'application/json',
+        success: function() {
+          app.fetch();
+          console.log('chatterbox: Message sent');
+        },
+        error: function(data) {
+          console.error('chatterbox: Failed to send message', data);
+        }
+      });
+    },
+
+    escapeArrayOfObjects: function(array) {
+      return array.map(function(obj) {
+        return _.mapObject(obj, str => _.escape(str));
+      });
+    },
+
+    renderMessageList: function() {
+      for (var i = messageList.length - 1; i >= 0; i--) {
+        $('#chats').prepend(app.generateMessageElement(messageList[i]));
+        latestMessageCreatedAt = messageList[i].createdAt;
       }
-    });
-  };
+    },
 
-  const postMessageToServer = function(username, text, roomname) {
-    $.ajax({
-      url: SERVER_URL,
-      type: 'POST',
-      data: JSON.stringify({username, text, roomname}),
-      contentType: 'application/json',
-      success: function() {
-        getMessagesFromServer();
-        console.log('chatterbox: Message sent');
-      },
-      error: function(data) {
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-  };
-
-  const escapeArrayOfObjects = function(array) {
-    return array.map(function(obj) {
-      return _.mapObject(obj, str => _.escape(str));
-    });
-  };
-
-  const renderMessageList = function() {
-    for (var i = messageList.length - 1; i >= 0; i--) {
-      $('#chats').prepend(generateMessageElement(messageList[i]));
-      latestMessageCreatedAt = messageList[i].createdAt;
+    generateMessageElement: function({ username, text }) {
+      return $(`<div class="chat"><div class="username">${username}</div><div class="message">${text}</div></div>`);
     }
   };
 
-  const generateMessageElement = function({ username, text }) {
-    return $(`<div class="chat"><div class="username">${username}</div><div class="message">${text}</div></div>`);
-  };
-
-  init();
-
+  app.init();
 });
-})();
+
